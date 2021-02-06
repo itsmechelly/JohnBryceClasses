01b043eafddc2bf9c4e246593022a4f2458b0746
hllyl <hlly.lov@gmail.com>
2021-02-06 16:52:48 +0200

    ...

---
+++ 10__SpringCore_Ec28_spring-lesson1/src/main/java/app/core/components/Animal.java
@@ -0,0 +1,11 @@
+package app.core.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Animal {
+
+	public void speak() {
+		System.out.println("animal speaks");
+	}
+}

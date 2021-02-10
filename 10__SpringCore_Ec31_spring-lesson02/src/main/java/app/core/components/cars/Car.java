3b42d28ec97d60530cac6cdf8292beff4e10c09f
hllyl <hlly.lov@gmail.com>
2021-02-10 18:22:04 +0200

    ...

--- 10__SpringCore_Ec31_spring-lesson02/src/main/java/app/core/components/cars/Car.java
+++ 10__SpringCore_Ec31_spring-lesson02/src/main/java/app/core/components/cars/Car.java
@@ -15,3 +15,5 @@ public class Car {
 //AAAUUUTTTOOOWWWIIIRRREEEDDD
 //AAAUUUTTTOOOWWWIIIRRREEEDDD
 //AAAUUUTTTOOOWWWIIIRRREEEDDD
+//333333333333333333333333333
+//When you have an object class that depends on other objects classes -> you will need to inject them in the object contractor, so you will be able to use it.

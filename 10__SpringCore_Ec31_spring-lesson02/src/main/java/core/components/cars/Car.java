d0cd4f701aedc192df6e0bd134840e4222a2d00f
hllyl <hlly.lov@gmail.com>
2021-02-11 15:16:30 +0200

    ...

--- 10__SpringCore_Ec31_spring-lesson02/src/main/java/core/components/cars/Car.java
+++ 10__SpringCore_Ec31_spring-lesson02/src/main/java/core/components/cars/Car.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class Car {
 
-	// In a case we don't know which engine we want, we can AutoWire it to make the spring network scan it with a new engine of your choice (components).
 	@Autowired
 	private Engine engine;
 	@Autowired
 	@Qualifier("basicGear") // Can also be used to set the primary qualifier.
-	private Gear basicGear; // If we set an instance name as a component name spring will use that as primary.
-	
+	private Gear basicGear; // If we set an instance name as a component name spring will use that as
+							// primary.
+
 	public void startCar() {
 		engine.start();
 		System.out.println("Car is on");
 	}
-	
+
 	public void startDrive() {
 		int state = basicGear.shiftUp();
 		System.out.println("Gear shifted to " + state);

1444aac404c44e84da95e81cc0ef31f7b43c18c6
hllyl <hlly.lov@gmail.com>
2021-02-11 09:21:23 +0200

    ...

--- 10__SpringCore_Ec31_spring-lesson02/src/main/java/app/core/components/cars/Car.java
+++ 10__SpringCore_Ec31_spring-lesson02/src/main/java/app/core/components/cars/Car.java
@@ -6,14 +6,3 @@ import org.springframework.stereotype.Component;
 public class Car {
 
 }
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//AAAUUUTTTOOOWWWIIIRRREEEDDD
-//333333333333333333333333333
-//When you have an object class that depends on other objects classes -> you will need to inject them in the object contractor, so you will be able to use it.

4f55e2a6c056eea5324c67b93595dcd391e98f4a
hllyl <hlly.lov@gmail.com>
2021-02-14 15:11:48 +0200

    ...

---
+++ 11__DesignPatterns_Ec32_AopProxy/src/Capitalizer.java
@@ -0,0 +1,5 @@
+
+public interface Capitalizer {
+
+	String modify(String text);
+}
---
+++ 11__DesignPatterns_Ec32_AopProxy/src/ConcreteCapitalizer.java
@@ -0,0 +1,9 @@
+
+public class ConcreteCapitalizer implements Capitalizer{
+
+	@Override
+	public String modify(String text) {
+		return text.toUpperCase();
+	}
+
+}

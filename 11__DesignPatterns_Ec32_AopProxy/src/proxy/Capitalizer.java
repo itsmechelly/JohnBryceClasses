2bf454d0b452f08aed1b1187e010bd364c372994
hllyl <hlly.lov@gmail.com>
2021-02-14 15:24:48 +0200

    ...

--- 11__DesignPatterns_Ec32_AopProxy/src/proxy/Capitalizer.java
+++ 11__DesignPatterns_Ec32_AopProxy/src/proxy/Capitalizer.java
@@ -1,3 +1,4 @@
+package proxy;
 
 public interface Capitalizer {
 
---
+++ 11__DesignPatterns_Ec32_AopProxy/src/proxy/CapitalizerCacheProxy.java
@@ -0,0 +1,25 @@
+package proxy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CapitalizerCacheProxy implements Capitalizer {
+
+	Map<String, String> cache = new HashMap<>();
+	ConcreteCapitalizer capitlizer = new ConcreteCapitalizer();
+
+	@Override
+	public String modify(String text) {
+		String value = cache.get(text);
+		// if that text is already in cache, use it.
+		if (value != null) {
+			return value;
+		}
+		// if that text is not in cache - process.
+		value = capitlizer.modify(text);
+		// save in cache for next time.
+		cache.put(text, value);
+		return value;
+	}
+
+}

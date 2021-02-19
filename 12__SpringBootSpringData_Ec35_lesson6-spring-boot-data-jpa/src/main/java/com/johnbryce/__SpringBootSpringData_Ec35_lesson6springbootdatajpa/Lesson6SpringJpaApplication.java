24ec0ff071a28bd59799cc2aa3d05e640f27163a
hllyl <hlly.lov@gmail.com>
2021-02-19 17:39:48 +0200

    ...

--- 12__SpringBootSpringData_Ec35_lesson6-spring-boot-data-jpa/src/main/java/com/johnbryce/__SpringBootSpringData_Ec35_lesson6springbootdatajpa/Lesson6SpringJpaApplication.java
+++ 12__SpringBootSpringData_Ec35_lesson6-spring-boot-data-jpa/src/main/java/com/johnbryce/__SpringBootSpringData_Ec35_lesson6springbootdatajpa/Lesson6SpringJpaApplication.java
@@ -2,12 +2,20 @@ package com.johnbryce.__SpringBootSpringData_Ec35_lesson6springbootdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import app.core.dao.StudentDAO;
+import app.core.entities.Student;
 
 @SpringBootApplication
-public class Application {
+public class Lesson6SpringJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(Lesson6SpringJpaApplication.class, args);) {
+			StudentDAO dao = ctx.getBean(StudentDAO.class);
+			dao.saveStudent(new Student("aaa", "aaa@mail"));
+		}
+
 	}
 
 }

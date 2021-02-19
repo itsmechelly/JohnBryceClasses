5131a1cc5fb48b7475076cfc6562596ce029c9c7
hllyl <hlly.lov@gmail.com>
2021-02-19 16:36:16 +0200

    ...

---
+++ 12__SpringBootSpringData_Ec35_lesson6-spring-boot-data-jpa/src/main/java/com/johnbryce/__SpringBootSpringData_Ec35_lesson6springbootdatajpa/Application.java
@@ -0,0 +1,13 @@
+package com.johnbryce.__SpringBootSpringData_Ec35_lesson6springbootdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 12__SpringBootSpringData_Ec35_lesson6-spring-boot-data-jpa/src/test/java/com/johnbryce/__SpringBootSpringData_Ec35_lesson6springbootdatajpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnbryce.__SpringBootSpringData_Ec35_lesson6springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

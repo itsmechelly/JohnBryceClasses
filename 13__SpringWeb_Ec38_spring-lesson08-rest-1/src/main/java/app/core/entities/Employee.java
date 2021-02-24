cf4a8c743f51aa955e119aff24540bda370a6ab0
hllyl <hlly.lov@gmail.com>
2021-02-24 15:43:34 +0200

    ...

---
+++ 13__SpringWeb_Ec38_spring-lesson08-rest-1/src/main/java/app/core/entities/Employee.java
@@ -0,0 +1,49 @@
+package app.core.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String role;
+	
+	public Employee() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+	
+}
---
+++ 13__SpringWeb_Ec38_spring-lesson08-rest-1/src/main/java/app/core/entities/repositories/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package app.core.entities.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepository {
+
+}

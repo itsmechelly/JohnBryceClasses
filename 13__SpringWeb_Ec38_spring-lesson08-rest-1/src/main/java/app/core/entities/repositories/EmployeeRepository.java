8e4cf0e2353d2a931c2c7c6ed6790685461c5c2a
hllyl <hlly.lov@gmail.com>
2021-02-24 16:46:01 +0200

    ...

--- 13__SpringWeb_Ec38_spring-lesson08-rest-1/src/main/java/app/core/entities/repositories/EmployeeRepository.java
+++ 13__SpringWeb_Ec38_spring-lesson08-rest-1/src/main/java/app/core/entities/repositories/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package app.core.entities.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import app.core.entities.Employee;
+
 @Repository
-public class EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }

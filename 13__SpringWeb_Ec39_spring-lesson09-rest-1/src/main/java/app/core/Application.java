b398514107d88fcf0fd780f1d4cf85fce6fa5b7a
hllyl <hlly.lov@gmail.com>
2021-03-02 12:23:32 +0200

    ...

--- 13__SpringWeb_Ec39_spring-lesson09-rest-1/src/main/java/app/core/Application.java
+++ 13__SpringWeb_Ec39_spring-lesson09-rest-1/src/main/java/app/core/Application.java
@@ -1,7 +1,12 @@
 package app.core;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import app.core.entities.Employee;
+import app.core.repositories.EmployeeRepository;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +15,26 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// define a CommandLineRunner bean for some start actions (will run on context
+	// startup)
+
+	@Bean // activate this bean if you wan the runner to run
+	CommandLineRunner initDBWithData(EmployeeRepository repo) {
+
+		// we can supply interface implementation using anonymous inner class:
+		CommandLineRunner runner = new CommandLineRunner() {
+
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println(">>> CommandLineRunner started");
+				// enter 2 employees to the database
+				repo.save(new Employee(0, "Ella", "Engineer"));
+				repo.save(new Employee(0, "Ron", "Legal Advisor"));
+
+			}
+		};
+
+		return runner;
+
+	}
 }
